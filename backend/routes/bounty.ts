adec67c17669624a6d56418919d64a77295fbee5
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-11-18 12:13:33 -0600

    merge

--- backend/routes/bounty.ts
+++ backend/routes/bounty.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from "express";
 import { db } from "../db";
-import { Bounty, BountySchema } from "../types";
+import { Bounty, BountyResponse, BountyResponseSchema, BountySchema } from "../types";
 
 const router = express.Router();
 
 async function getBounty(req: Request, res: Response) {
   try {
-    const collection = db.collection<Bounty>("bounties");
+    const collection = db.collection<Bounty>("bounty");
     const bountyItems = await collection.find({}).toArray();
     res.status(200).json(bountyItems);
   } catch (error: any) {
@@ -19,7 +19,7 @@ router.get("/bounty", getBounty);
 async function postBounty(req: Request, res: Response) {
   try {
     const newBounty = BountySchema.parse(req.body);
-    const collection = db.collection<Bounty>("bounties");
+    const collection = db.collection<Bounty>("bounty");
     const result = await collection.insertOne(newBounty);
     res.status(201).json(result);
   } catch (error: any) {
@@ -28,5 +28,16 @@ async function postBounty(req: Request, res: Response) {
 }
 
 router.post("/bounty", postBounty);
+
+async function postBountyResponse(req: Request, res: Response) {
+  try {
+    const newBountyResponse = BountyResponseSchema.parse(req.body);
+    const collection = db.collection<BountyResponse>("bountyResponse");
+    const result = await collection.insertOne(newBountyResponse);
+    res.status(201).json(result);
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+}
 
 export default router;
--- backend/types.ts
+++ backend/types.ts
@@ -9,7 +9,18 @@ export const BountySchema = z.object({
   }),
   description: z.string(),
   created_at: z.date(),
+  
 });
 
 // Type alias for the validated data
 export type Bounty = z.infer<typeof BountySchema>;
+
+export const BountyResponseSchema = z.object({
+  bounty_id: z.string(),
+  answerer_user_id: z.string(),
+  answer_content: z.string(),
+  images: z.array(z.string()), // Array of image URLs
+  created_at: z.date(),
+});
+
+export type BountyResponse = z.infer<typeof BountyResponseSchema>;
